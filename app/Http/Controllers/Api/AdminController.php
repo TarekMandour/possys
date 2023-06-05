499efce1b7bf508dd76ed731685977724d92b8db
TarekMandour <81233440+TarekMandour@users.noreply.github.com>
2023-06-05 17:03:54 +0200

    t

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -348,6 +348,13 @@ class AdminController extends Controller
             $data = Post::where('title', 'like', '%' . $request->key . '%')
                 ->orWhere('title_en', 'like', '%' . $request->key . '%')
                 ->orWhere('content','like','%' . $request->key . '%')
+                ->with(['stock'=>function($query) use($branch_id){
+                    $query->where('branch_id',$branch_id);
+                    $query->Where('qty', '!=' , 0);
+                    $query->Where('qty', '>' , 0);
+                    $query->orWhere('qty_mid', '!=' , 0);
+                    $query->orWhere('qty_sm', '!=' , 0);
+                }])
                 ->paginate(10);
         } else {
             $data = Category::where('title', 'like', '%' . $request->key . '%')
